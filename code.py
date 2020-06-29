95071545f85dcebd3955a417608a0959f3c120f2
kenny <kenny@sourcetonuts.com>
2020-06-29 13:37:53 -0400

    cleanup and commenting

--- code.py
+++ code.py
@@ -12,17 +12,10 @@ touch = touchio.TouchIn( board.A0 )
 # refer to
 # https://learn.adafruit.com/fancyled-library-for-circuitpython/led-colors
 #
-palette = [fancy.CHSV(1.0, 1.0, 1.0),
-           fancy.CHSV(0.8, 1.0, 1.0),
-           fancy.CHSV(0.3, 1.0, 1.0),
-           fancy.CHSV(0.0, 1.0, 1.0),
-           fancy.CHSV(0.3, 1.0, 1.0),
-           fancy.CHSV(0.8, 1.0, 1.0)]
-
 grad = [ (0.0,0xFF0000), (0.33,0x00FF00), (0.67,0x0000FF), (1.0,0xFF0000)]
-#grad = [(0.0, 0x97461A), (0.3, 0xFBD8C5), (0.83, 0x6C2E16), (1.0, 0xEFDBCD)]
 palette = fancy.expand_gradient( grad, 50 )
 
+# todo read in these as stored from NVM or Drive or ?
 onoff = True
 offset = 0
 
@@ -45,16 +38,19 @@ while True :
 
     time.sleep(0.005)  # 5ms delay
     if not wason and touch.value :
+        # just touched mode button
         onoff = not onoff # toggle onoff state
 
         # when off paint/fill w/ the center color
         if onoff :
+            # flash back before starting rainbow
             strip.fill( (0,0,0) )
             strip.show()
         else :
+            # pick the center color and fill the strip with that color statically displayed
             colorindex = offset + 0.5
             color = fancy.palette_lookup( palette, colorindex )
             strip.fill( color.pack() )
             strip.show()
 
-        time.sleep(0.5)  # big delay so we dont 2x trigger
+        time.sleep( 0.5 )  # big delay so we dont 2x trigger
