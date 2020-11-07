e78c32afd62f9be7ca464a7b4c4138a760d5fa56
kenny <kenny@sourcetonuts.com>
2020-11-07 10:46:44 -0500

    working fancypole on gemma m0

--- code.py
+++ code.py
@@ -1,120 +1,102 @@
 import time
 import board
-import microcontroller
 import touchio
-from digitalio import DigitalInOut, Direction, Pull
 import neopixel
 import adafruit_fancyled.adafruit_fancyled as fancy
 
+num_pixels = 30
 
-# TRINKET M0
-# with the following 96 LED setup on an Adafruit Trinket M0
-# it could be wired up to run off of a basic/standard USB charger or laptop
-# cabling required for Trinket
-#   USB micro for power
-#   pin 1 to touch button/copper pad
-#   pin 4 to NeoPixel data input pin
-#   along with USB and GND from the Trinket for NeoPixel power/ground/data-in 3 conductor cable
+# pin usage: TRINKET: board.D4, GEMMA: board.D1
+strip = neopixel.NeoPixel( board.D1, num_pixels, brightness = 0.25, auto_write = False )
 
-# GEMMA M0 wiring
-#   USB micro for power
-#   pin 6 aka A2 to touch button/copper pad
-#   pin 2 aka D1 to NeoPixel data input pin
-#   along with USB and GND from the Trinket for NeoPixel power/ground/data-in 3 conductor cable
-
-num_pixels = 96
-led_brightness = 0.25 # fraction of full power (TBD mamps
-pin_leddata = board.D1   # Trinket M0 board.D4
-pin_touch = board.A1     # Trinket M0 board.A0
-print( "FancyPole" )
-
-strip = neopixel.NeoPixel( pin_leddata, num_pixels, brightness = led_brightness, auto_write = False )
-touch = touchio.TouchIn( pin_touch )
+print( "FancyPole Gemma M0" )
 
 # refer to
 # https://learn.adafruit.com/fancyled-library-for-circuitpython/led-colors
-
 # across the rainbow
 grad = [ (0.0,0xFF0000), (0.33,0x00FF00), (0.67,0x0000FF), (1.0,0xFF0000)]
-
-# nice set of orange
-#grad = [ (0.0,0x708800), (0.1,0x305500), (0.5,0x001100), (0.9,0x305500), (1.0,0x708800)]
-
-# shades of gray
-#grad = [ (0.0,0xf0f0f0), (0.5,0x101010), (1.0,0xf0f0f0)]
-
-# gold
-#grad = [ (0.0,0xffdf00), (0.5,0xc5b358), (1.0,0xffdf00)]
-
-
 palette = fancy.expand_gradient( grad, 20 )
 
-# todo read in these as stored from NVM or Drive or ?
+#
+# returns the mid color in the palette
+# which we call the selected color
+#
+def color_selected( coloff ) :
+    # pick the center color and fill the strip with that color statically displayed
+    colorindex = coloff + 0.5
+    color = fancy.palette_lookup( palette, colorindex )
+    return color.pack()
+
+#
+# sets the strip to the color offset into the palette
+#
+def show_static( coloff ) :
+    rgb = color_selected( coloff )
+    strip.fill( rgb )
+    strip.show()
+
+#
+# cycles through the palette starting w/ an offset into the palette
+# show all of the palette, so loops back to the start based on the
+# starting offset
+#
+def palette_cycle( coloff ) :
+    for i in range( num_pixels ) :
+        colorindex = coloff + ( i / num_pixels )
+        color = fancy.palette_lookup( palette, colorindex )
+        rgb = color.pack()
+        strip[i] = rgb
+    strip.show()
+
+# TouchMode
+# this class manages a single captouch pin and cycles through modes
+# on that pin up to provided lastmode
+#
+class TouchMode :
+    def __init__( self, pin, lastmode = 1, name = None ) :
+        self.mode = 0
+        self.lastmode = lastmode
+        self.touch = touchio.TouchIn( pin )
+        self.name = name or "mode"
+
+    def exec( self ) :
+        wasoff = not self.touch.value
+        time.sleep(0.005)  # 5ms delay for debounce
+        if not wasoff and self.touch.value :
+            # just touched button
+            time.sleep(0.005)  # 5ms delay for debounce
+            if self.mode >= self.lastmode :
+                self.mode = 0
+            else :
+                self.mode = self.mode + 1
+            # debounce
+            time.sleep( 0.5 )
+            print( self.name + " {}".format( self.mode ) )
+        return self.mode
+
+# OnOff pin usage: TRINKET: board.A0, GEMMA: board.A1
+# Mode pin usage: TRINKET: board. TBD ??, GEMMA: board.A2
+
+onoffMachine = TouchMode( board.A1, 1, "onoff" )
+modeMachine = TouchMode( board.A2, 2 )
+
 onoff = True
 offset = 0.001
-
-def show_static() :
-    print("show_static()")
-    # pick the center color and fill the strip with that color statically displayed
-    colorindex = offset + 0.5
-    color = fancy.palette_lookup( palette, colorindex )
-    strip.fill( color.pack() )
-    strip.show()
-    print( "offset: {}".format(offset) )
-    microcontroller.nvm[0] = int( offset * num_pixels ) % num_pixels
-
-def remember_settings() :
-    stored = microcontroller.nvm[0]
-    print("remember_settings({})".format(stored) )
-    if stored == 0 :
-        onoff = True
-        offset = 0.001
-    elif stored > num_pixels :
-        onoff = True
-        offset = 0.001
-    else :
-        onoff = False
-        offset = stored / num_pixels
-        show_static()
-
-def palette_cycle() :
-    for i in range( num_pixels ) :
-        colorindex = offset + ( i / num_pixels )
-        color = fancy.palette_lookup( palette, colorindex )
-        strip[i] = color.pack()
-        if touch.value :
-            return
-    strip.show()
-
-def restart_rainbow() :
-    print("restart_rainbow()")
-    # flash back before starting rainbow
-    #strip.fill( (0,0,0) )
-    #strip.show()
-
-remember_settings()
+mode = 0
 
 # Loop Forever
 while True :
-    if onoff :
-        # cycle the rainbow when on
-        palette_cycle()
+    OnOff = onoffMachine.exec()
+    if OnOff == 0 :
+        palette_cycle( offset )
         offset += 0.035 # this sets how quickly the rainbow changes (bigger is faster)
+    else :
+        newmode = modeMachine.exec()
+        if mode != newmode :
+            mode = newmode
 
-    # deal w/ button presses...
-    wason = not touch.value
-    time.sleep(0.005)  # 5ms delay for debounce
-    if not wason and touch.value :
-        # just touched mode button
-        time.sleep(0.005)  # 5ms delay for debounce
-        onoff = not onoff # toggle onoff state
-
-        if onoff :
-            # when just on restart color cycling
-            restart_rainbow()
-            microcontroller.nvm[0] = 0 # zero offset stored changing rainbow is active
-        else :
+        if mode == 0 :
             # and if just off just off paint/fill w/ the center color
-            show_static()
-
-        time.sleep( 0.5 )  # big delay so we dont 2x trigger button presses
+            show_static( offset )
+        else :
+            show_static( offset )
