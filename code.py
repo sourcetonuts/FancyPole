b1653977d64dc5ae4ac5401133cb4f4a86e27cc9
Kenny Lindberg <kenny@mixtroz.com>
2021-04-24 16:22:14 -0400

    not yet tested but ported to dotsart and mypy library

--- code.py
+++ code.py
@@ -1,117 +1,37 @@
-import time
 import board
-import microcontroller
 import touchio
-from digitalio import DigitalInOut, Direction, Pull
-import adafruit_dotstar as dotstar
-import adafruit_fancyled.adafruit_fancyled as fancy
+import neopixel
 
-# with the following 96 LED setup on an Adafruit Trinket M0
-# it could be wired up to run off of a basic/standard USB charger or laptop
-# cabling required for Trinket
-#   USB micro for power
-#   pin 1 to touch button/copper pad
-#   pin 3 to Dotstar clock input pin
-#   pin 4 to Dotstar data input pin
-#   along with USB and GND from the Trinket for NeoPixel power/ground/data-in 3 conductor cable
+print( "FancyPole #2 Trinket M0" )
 
-num_pixels = 96
-led_brightness = 0.25 # fraction of full power (TBD mamps
-pin_touch = board.A0
-pin_status = board.D13
-print( "FancyPole" )
+# make the strip and here a 96 LED NeoPixel strip (can be dotstar, etc. w/ libraries)
+strip = neopixel.NeoPixel(
+    board.D4, 96, brightness = 0.25,
+    auto_write = False, pixel_order= neopixel.RGB )
 
-strip = dotstar.DotStar( board.SCK, board.MOSI, num_pixels, brightness=led_brightness, auto_write = False )
-touch = touchio.TouchIn( pin_touch )
-status = DigitalInOut( pin_status )
-status.direction = Direction.OUTPUT
+# Kenny's Display classs, It uses strip passed and libraries: adafruit_fancyled
+import MyPy.rainman
+display = MyPy.rainman.RainMan( strip )
 
-# refer to
-# https://learn.adafruit.com/fancyled-library-for-circuitpython/led-colors
+# Kenny's TouchMode class, It uses touchio passed and libraries: time
+import MyPy.touchmode
 
-# across the rainbow
-grad = [ (0.0,0xFF0000), (0.33,0x00FF00), (0.67,0x0000FF), (1.0,0xFF0000)]
+# handles the application's Mode:
+# 0:rainbow select
+# 1:display selected color
+inputMode = touchio.TouchIn( board.A0 )
+modeMachine = MyPy.touchmode.TouchMode( inputMode, 2 )
 
-# nice set of orange
-#grad = [ (0.0,0x708800), (0.1,0x305500), (0.5,0x001100), (0.9,0x305500), (1.0,0x708800)]
-
-# shades of gray
-#grad = [ (0.0,0xf0f0f0), (0.5,0x101010), (1.0,0xf0f0f0)]
-
-# gold
-#grad = [ (0.0,0xffdf00), (0.5,0xc5b358), (1.0,0xffdf00)]
-
-
-palette = fancy.expand_gradient( grad, 20 )
-
-# todo read in these as stored from NVM or Drive or ?
-onoff = True
-offset = 0.001
-
-def show_static() :
-    print("show_static()")
-    # pick the center color and fill the strip with that color statically displayed
-    colorindex = offset + 0.5
-    color = fancy.palette_lookup( palette, colorindex )
-    strip.fill( color.pack() )
-    strip.show()
-    print( "offset: {}".format(offset) )
-    microcontroller.nvm[0] = int( offset * num_pixels ) % num_pixels
-
-def remember_settings() :
-    stored = microcontroller.nvm[0]
-    print("remember_settings({})".format(stored) )
-    if stored == 0 :
-        onoff = True
-        offset = 0.001
-    elif stored > num_pixels :
-        onoff = True
-        offset = 0.001
-    else :
-        onoff = False
-        offset = stored / num_pixels
-        show_static()
-
-def palette_cycle() :
-    for i in range( num_pixels ):
-        colorindex = offset + ( i / num_pixels )
-        color = fancy.palette_lookup( palette, colorindex )
-        strip[i] = color.pack()
-        if touch.value :
-            return
-    strip.show()
-
-def restart_rainbow() :
-    print("restart_rainbow()")
-    # flash back before starting rainbow
-    #strip.fill( (0,0,0) )
-    #strip.show()
-
-remember_settings()
-status.value = onoff
+offset = 0
 
 # Loop Forever
 while True :
-    if onoff :
-        # cycle the rainbow when on
-        palette_cycle()
-        offset += 0.035 # this sets how quickly the rainbow changes (bigger is faster)
+    mode = modeMachine.update()
+    if mode == 0 :
+        display.palette_cycle( offset )
+        offset += 0.005 # raising this speeds up the changes/steps
+    else :
+        # just paint/fill w/ the center color
+        display.show_static( offset + 0.5 )
 
-    # deal w/ button presses...
-    wason = not touch.value
-    time.sleep(0.005)  # 5ms delay for debounce
-    if not wason and touch.value :
-        # just touched mode button
-        time.sleep(0.005)  # 5ms delay for debounce
-        onoff = not onoff # toggle onoff state
-        status.value = onoff
-
-        if onoff :
-            # when just on restart color cycling
-            restart_rainbow()
-            microcontroller.nvm[0] = 0 # zero offset stored changing rainbow is active
-        else :
-            # and if just off just off paint/fill w/ the center color
-            show_static()
-
-        time.sleep( 0.5 )  # big delay so we dont 2x trigger button presses
+# end of program
